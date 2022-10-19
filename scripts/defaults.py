cfd54220aaced78e5d5994c7bdfcd4f5f71a028f
Fisseha Estifanos <fisseha.137@gmail.com>
2022-10-19 16:00:00 +0300

    defaults: script created for default values

---
+++ scripts/defaults.py
@@ -0,0 +1,10 @@
+"""
+A default parameters and variable holder script
+"""
+
+# base repo
+repo = '../'
+
+# data  files
+data1 = 'data/data1_completed_orders.csv'
+data2 = 'data/data2_driver_locations_during_request.csv'
